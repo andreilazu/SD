5b929ff2dd09e41a67e3a6170e3557daabfb7084
andreilazu <lazureanuandrei01@gmail.com>
2026-06-02 21:03:03 +0300

    Update problema1.cpp

--- lab6/problema1.cpp
+++ lab6/problema1.cpp
@@ -2,143 +2,54 @@
 RMQ
 */
 #include <iostream>
-#include <vector>
-#include <string>
-#include <unordered_map>
-#include <map>
 #include <fstream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <bitset>
+#include <math.h>  
 
 using namespace std;
 
-const int LOG = 20;
+ifstream in("rmq.in");
+ofstream out("rmq.out");
 
-struct Node {
-    map<char, int> next;
-    int pass = 0;
-    int end = 0;
-};
+#define cin in
+#define cout out
 
-struct Trie {
-    vector<Node> v;
-    unordered_map<int, int> tin;
-    vector<vector<int>> up;
-    vector<int> parent;
+const int N = 1e5 + 1, LOGN = 20;
 
-    Trie() {
-        v.push_back({});
-        up.push_back(vector<int>(LOG, 0));
-        parent.push_back(0);
-    }
+int A[N], lg[N];
+int M[N][LOGN];
 
-    int insert(const string& s)
-    {
-        int node = 0;
-
-        for (char c : s)
-        {
-            if (!v[node].next.count(c))
-            {
-                v[node].next[c] = v.size();
-                v.push_back({});
-                up.push_back(vector<int>(LOG, 0));
-                parent.push_back(node);
-            }
-
-            node = v[node].next[c];
-        }
-
-        v[node].end++;
-        return node;
-    }
-
-    void dfs(int node, int depth = 0)
-    {
-        tin[node] = depth;
-
-        for (int i = 1; i < LOG; i++)
-            up[node][i] = up[up[node][i - 1]][i - 1];
-
-        for (auto& p : v[node].next)
-        {
-            int child = p.second;
-            up[child][0] = node;
-            dfs(child, depth + 1);
-        }
-    }
-
-    int lca(int a, int b)
-    {
-        if (tin[a] < tin[b])
-            swap(a, b);
-
-        int diff = tin[a] - tin[b];
-
-        for (int k = LOG - 1; k >= 0; k--)
-            if (diff & (1 << k))
-                a = up[a][k];
-
-        if (a == b)
-            return a;
-
-        for (int k = LOG - 1; k >= 0; k--)
-        {
-            if (up[a][k] != up[b][k])
-            {
-                a = up[a][k];
-                b = up[b][k];
-            }
-        }
-
-        return up[a][0];
-    }
-
-    int query(vector<int>& nodes)
-    {
-        int cur = nodes[0];
-
-        for (int i = 1; i < nodes.size(); i++)
-            cur = lca(cur, nodes[i]);
-
-        return tin[cur];
-    }
-};
+int n, q;
 
 int main()
 {
-    ifstream fin("ratina.in");
-    ofstream fout("ratina.out");
-
-    int N, M;
-    fin >> N >> M;
-
-    Trie t;
-    vector<int> wordNode(N);
-
-    for (int i = 0; i < N; i++)
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++)
     {
-        string s;
-        fin >> s;
-        wordNode[i] = t.insert(s);
+        cin >> A[i];
+        M[i][0] = i;
     }
-
-    t.dfs(0, 0);
-
-    while (M--)
+    A[0] = 9999999;
+    for (int j = 1; (1 << j) <= n; j++)
     {
-        int k;
-        fin >> k;
-
-        vector<int> nodes(k);
-
-        for (int i = 0; i < k; i++)
+        for (int i = 1; i + (1 << (j - 1)) <= n; i++)
         {
-            int x;
-            fin >> x;
-            nodes[i] = wordNode[x - 1];
+            if (A[M[i][j - 1]] < A[M[i + (1 << (j - 1))][j - 1]])
+            {
+                M[i][j] = M[i][j - 1];
+            }
+            else
+                M[i][j] = M[i + (1 << (j - 1))][j - 1];
         }
-
-        fout << t.query(nodes) << "\n";
     }
-
-    return 0;
+    for (int i = 1; i <= q; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        int k = log2(b - a + 1);
+        cout << min(A[M[a][k]], A[M[b - (1 << k) + 1][k]]) << '\n';
+    }
 }
